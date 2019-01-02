64f89fd04614ed4ada0037a6a4ae2e71dc25c628
Yahor Laurenau <Yahor_Laurenau@epam.com>
2019-01-02 13:19:15 +0300

    Mappers test was added

---
+++ BLL.WebApi.Tests/DalMappingTests.cs
@@ -0,0 +1,57 @@
+﻿using BLL.WebApi.Models.ViewModels;
+using DAL.CosmosDb.Entities;
+using DAL.CosmosDb.Mapper;
+using DAL.Interfaces.DTO;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace BLL.WebApi.Tests
+{
+    [TestFixture()]
+    public class DalMappingTests
+    {
+        private Item _mapperTester = new Item
+        {
+            Id = "11",
+            Make = "Ford",
+            Model = "Garrs",
+            Colour = "Blue",
+            Features = new ItemFeatures()
+            {
+                AirConditioning = true,
+                HeatingSeats = false,
+                AudioPlayer = false,
+                CupHolder = false
+            }
+        };
+
+        [Test]
+        public void WebApiMapperTest_ReturnTrueIfMappingWasMade()
+        {
+            var model = Mapper.ToCarDto(_mapperTester);
+
+            if (isTrueMapper(model, _mapperTester))
+            {
+                Assert.Pass();
+            }
+
+            Assert.Fail();
+        }
+        private bool isTrueMapper(Car carModel, Item itemModel)
+        {
+            if (carModel.Id != itemModel.Id) return false;
+            if (carModel.Colour != itemModel.Colour) return false;
+            if (carModel.Make != itemModel.Make) return false;
+            if (carModel.Model != itemModel.Model) return false;
+            return isTrueFeatureModel(carModel.Features, itemModel.Features);
+        }
+
+        private bool isTrueFeatureModel(CarFeatures carFeatures, ItemFeatures itemFeatures)
+        {
+            if (carFeatures.AirConditioning != itemFeatures.AirConditioning) return false;
+            if (carFeatures.AudioPlayer != itemFeatures.AudioPlayer) return false;
+            if (carFeatures.CupHolder != itemFeatures.CupHolder) return false;
+            return carFeatures.HeatingSeats == itemFeatures.HeatingSeats;
+        }
+    }
+}
---
+++ BLL.WebApi.Tests/WebApiMappingTests.cs
@@ -0,0 +1,58 @@
+﻿using BLL.WebApi.Mappers;
+using BLL.WebApi.Models.ViewModels;
+using DAL.Interfaces.DTO;
+using NUnit.Framework;
+using Assert = NUnit.Framework.Assert;
+
+
+namespace BLL.WebApi.Tests
+{
+    [TestFixture]
+    public class MapperTests
+    {
+        private Car _mapperTester = new Car
+        {
+            Id = "11",
+            Make = "Ford",
+            Model = "Garrs",
+            Colour = "Blue",
+            Features = new CarFeatures()
+            {
+                AirConditioning = true,
+                HeatingSeats = false,
+                AudioPlayer = false,
+                CupHolder = false
+            }
+        };
+
+        [Test]
+        public void WebApiMapperTest_ReturnTrueIfMappingWasMade()
+        {
+            var carView = Mapper.ToCarViewModel(_mapperTester);
+
+            if (isTrueMapper(carView,_mapperTester))
+            {
+                Assert.Pass();
+            }
+            
+            Assert.Fail();
+        }
+
+        private bool isTrueMapper(CarViewModel viewModel, Car carModel)
+        {
+            if (viewModel.Id != carModel.Id) return false;
+            if (viewModel.Colour != carModel.Colour) return false;
+            if (viewModel.Make != carModel.Make) return false;
+            if (viewModel.Model != carModel.Model) return false;
+            return isTrueFeatureModel(viewModel.Features, carModel.Features);
+        }
+
+        private bool isTrueFeatureModel(CarFeaturesViewModel viewModel, CarFeatures carModel)
+        {
+            if (viewModel.AirConditioning != carModel.AirConditioning) return false;
+            if (viewModel.AudioPlayer != carModel.AudioPlayer) return false;
+            if (viewModel.CupHolder != carModel.CupHolder) return false;
+            return viewModel.HeatingSeats == carModel.HeatingSeats;
+        }
+    }
+}
